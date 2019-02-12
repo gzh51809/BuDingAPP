2f1db24ec4fb4a60f60a9bfb5ea0435cfa17e222
435982566 <435982566@qq.com>
2019-02-12 20:34:32 +0800

    11

--- src/com/Home.js
+++ src/com/Home.js
@@ -1,12 +1,108 @@
 import React from 'react';
+import './css/home.css';
+import { Carousel, WingBlank } from 'antd-mobile';
+import { Grid } from 'antd-mobile';
 
 
-export default class Home extends React.Component {
+
+
+class home extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text: props.initialValue || 'placeholder',
+            imglist: { a: require('./img/home-1.jpg'), b: require('./img/home-2.jpg') },
+            arr: ['1', '2'],
+            imgHeight: 176,
+            arri:[{
+                icon:require( './img/addShapefile-icon.png'),
+                text: `旅游`
+            },{
+                icon:require('./img/hunyushouyang-jiaotongchuhang-jiaoyujiuxue-.png'),
+                text: `机票火车票`
+            },{
+                icon:require( './img/jifenshangcheng.png'),
+                text: `积分商城`
+            },{
+                icon:require('./img/qiandao.png'),
+                text: `签到有礼`
+            },{
+                icon:require('./img/choujiang.png'),
+                text: `预定抽奖`
+            },{
+                icon:require('./img/552cc40952152.png'), 
+                text: `优惠卷`
+            },{
+                icon:require('./img/huafeichongzhi.png'), 
+                text: `话费充值`
+            },{
+                icon:require('./img/shenduyou.png'), 
+                text: `深度游`
+            }]
+        };
+    }
+    componentDidMount() {
+        // simulate img loading
+        setTimeout(() => {
+            this.setState({
+                arr: [this.state.imglist.a, this.state.imglist.b],
+            });
+        }, 100);
+    }
     render() {
         return (
-            <div>
-                <p>去detail</p>
-            </div>
-        )
+            <WingBlank>
+                <Carousel
+                    autoplay={true}
+                    infinite
+                    beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+                    afterChange={index => console.log('slide to', index)}
+                >
+                    {this.state.arr.map(val => (
+                        <a
+                            key={val}
+                            href="http://www.alipay.com"
+                            style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                        >
+                            <img
+                                src={val}
+                                alt=""
+                                style={{ width: '100%', verticalAlign: 'top' }}
+                                onLoad={() => {
+                                    // fire window resize event to change height
+                                    window.dispatchEvent(new Event('resize'));
+                                    this.setState({ imgHeight: 'auto' });
+                                }}
+                            />
+                        </a>
+                    ))}
+                </Carousel>
+                <Grid data={this.state.arri} isCarousel onClick={_el => console.log(_el)} />
+
+                <WingBlank>
+                    <div className="Carousel">
+                        <div className="cou"><p>热门活动:</p></div>
+                        <Carousel className="my-carousel"
+                            vertical
+                            dots={false}
+                            dragging={false}
+                            swiping={false}
+                            autoplay
+                            infinite
+                        >
+                            <div className="v-item">疯狂拆福袋</div>
+                            <div className="v-item">首次住69元起</div>
+                            <div className="v-item">新开门店39元起</div>
+                        </Carousel>
+                    </div>
+                </WingBlank>
+
+            </WingBlank>
+
+        );
     }
 }
+
+
+export default home;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,102 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import {HashRouter, Route, Switch} from 'react-router-dom';
+import Router from './router/router';
+
 import Home from './com/home';
 import Detail from './com/detail';
-import App from './com/App';
+import List from './com/list';
 
 
-const BasicRoute = () => (
-    <HashRouter>
-        <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/detail" component={Detail}/>
-            <Route exact path="/App" component={Detail}/>
-        </Switch>
-    </HashRouter>
+ReactDOM.render(
+    <Router />,
+    document.getElementById('root')
 );
+class index extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            menu: [
+                {
+                    text: '首页',
+                    path: '/home',
+                    name: 'Home',
+                    icon: 'home'
+                }, {
+                    text: '列表',
+                    path: '/detail',
+                    name: 'Detail;',
+                    icon: 'detail'
+                }, {
+                    text: '列表',
+                    path: '/list',
+                    name: 'List;',
+                    icon: 'list'
+                }
+            ],
+            current: '/home'
+
+        }
+
+        // this绑定
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange({ item, key, keyPath }) {
+        //两个问题：1、如何获取路由路径，2、如何获取history对象
+        this.setState({
+            current: key
+        });
+
+        // console.log(this.props.history);
+
+        // 
+        this.props.history.push(key)
+    }
+    componentDidMount() {
+
+        // 利用生命周期函数来保持当前路由高亮
+        // 获取当前路由（hash,history）
+        let hash = window.location.hash;// 可能得到的值：/home,/list,/list/computer
+        hash = hash.split('/')[1];
+
+        this.setState({
+            current: '/' + hash
+        })
+
+    }
+    render() {
+        return (
+            <div>
+                <h1>React Router路由演示</h1>
+                <menu
+                    mode="horizontal"
+                    selectedKeys={[this.state.current]}
+                    onClick={this.handleChange}
+                >
+                    {
+                        this.state.menu.map(menu => {
+                            return (
+                                <menu.Item key={menu.path}>
+                                    <icon type={menu.icon} />{menu.text}
+                                </menu.Item>
+                            )
+                        })
+                    }
+                </menu>
+                <switch>
+                    <route path="/home" component={Home} />
+                    <route path="/list" component={List} />
+                    <route path="/detail" component={Detail} />
+                    <redirect from="/" to="/home" />
+                    {/* <Route path="/" component={Home} exact/> */}
+                </switch>
+            </div>
+        )
+    }
+}
+export default index;
 
 
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
-
-export default BasicRoute;
